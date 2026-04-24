57821b97d669943d6e1ad22909b833f263f51153
lephanquocdai <qdai1209@gmail.com>
2026-04-24 23:55:08 +0700

    update bai moi

--- JavaBootcamp/src/com/bootcamp/Thi1/CalculatorV2.java
+++ JavaBootcamp/src/com/bootcamp/Thi1/CalculatorV2.java
@@ -4,23 +4,23 @@ import java.util.Scanner;
 
 public class CalculatorV2 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
        System.out.println("===May Tinh Ca Nhan==="); 
        System.out.print("Nhap so1: ");
-       double so1 = scanner.nextDouble();
+       double so1 = sc.nextDouble();
 
        System.out.print("Nhap so2: ");
-       double so2 = scanner.nextDouble();
+       double so2 = sc.nextDouble();
 
        System.out.print("Chon phep toan op (+,-,*,/): ");
-       char phepTinh = scanner.next().charAt(0);
-       scanner.nextLine(); // Đọc bỏ dòng trống sau khi nhập phepTinh
+       char phepTinh = sc.next().charAt(0);
+       sc.nextLine(); // Đọc bỏ dòng trống sau khi nhập phepTinh
 
        double ketQua = 0;
        boolean thanhCong = true;
        System.out.print("Nhap ghi chu: ");
-       String ghiChu = scanner.nextLine();
+       String ghiChu = sc.nextLine();
 
        switch (phepTinh) {
             case '+':
@@ -36,7 +36,7 @@ public class CalculatorV2 {
                 while (so2 == 0) {
                     System.out.println("LOI: KHONG THE CHIA CHO 0. VUI LONG NHAP LAI SO2!");
                     System.out.print("Nhap lai so2: ");
-                    so2 = scanner.nextDouble();
+                    so2 = sc.nextDouble();
                 }
                 ketQua = so1 / so2;
                 break;
@@ -49,6 +49,6 @@ public class CalculatorV2 {
             System.out.printf("KET QUA: %.2f %c %.2f = %.2f\n", so1, phepTinh, so2, ketQua);
             System.out.println("GHI CHU: " + ghiChu);
         }
-        scanner.close();
+        sc.close();
     }
 }
