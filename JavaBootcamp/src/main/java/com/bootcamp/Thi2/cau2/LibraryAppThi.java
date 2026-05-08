3f79f79a0d19bbd86ac66196b1919be673cca289
lephanquocdai <qdai1209@gmail.com>
2026-05-08 23:42:46 +0700

    fix th2 cau2

--- JavaBootcamp/src/main/java/com/bootcamp/Thi2/cau2/LibraryAppThi.java
+++ JavaBootcamp/src/main/java/com/bootcamp/Thi2/cau2/LibraryAppThi.java
@@ -5,27 +5,28 @@ import java.util.Scanner;
 public class LibraryAppThi {
     public static void main(String[] args) {
 
-        Scanner scn = new Scanner(System.in);
-        int d;
-        BorrowableDoc[] bDocs = new BorrowableDoc[4];
+        try (Scanner scn = new Scanner(System.in)) {
+            int d;
+            BorrowableDoc[] bDocs = new BorrowableDoc[4];
 
-        bDocs[0] = new BookDoc("Nam Cao", "M1");
-        bDocs[1] = new MagazineDoc("Mo vi sao", "M2");
-        bDocs[2] = new BookDoc("Truyen Kieu", "M3");
-        bDocs[3] = new MagazineDoc("To Huu", "M4");
+            bDocs[0] = new BookDoc("Nam Cao", "M1");
+            bDocs[1] = new MagazineDoc("Mo vi sao", "M2");
+            bDocs[2] = new BookDoc("Truyen Kieu", "M3");
+            bDocs[3] = new MagazineDoc("To Huu", "M4");
 
-        do {
-            System.out.print("Nhap so ngay muon: ");
-            d = scn.nextInt();
-        } while (d < 1);
+            do {
+                System.out.print("Nhap so ngay muon: ");
+                d = scn.nextInt();
+            } while (d < 1);
 
 
-        double totalPhi = 0;
-        for (BorrowableDoc bDoc : bDocs) {
-            bDoc.display(d);
-            totalPhi += bDoc.getBorrowFee(d);
-        }
-        System.out.println("\n=====Tong phi====="); 
-        System.out.printf("Tong phi: %.0f VND\n" , totalPhi); 
+            double totalPhi = 0;
+            for (BorrowableDoc bDoc : bDocs) {
+                bDoc.display(d);
+                totalPhi += bDoc.getBorrowFee(d);
+            }
+            System.out.println("\n=====Tong phi====="); 
+            System.out.printf("Tong phi: %.0f VND\n" , totalPhi);
+        } 
     }
 }
