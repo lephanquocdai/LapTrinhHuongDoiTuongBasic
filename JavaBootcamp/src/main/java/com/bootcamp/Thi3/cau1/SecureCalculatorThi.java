da5c99028f4e97bbe40057bbaad9db5dfcfbd57f
lephanquocdai <qdai1209@gmail.com>
2026-05-11 20:49:57 +0700

    fix font

--- JavaBootcamp/src/main/java/com/bootcamp/Thi3/cau1/SecureCalculatorThi.java
+++ JavaBootcamp/src/main/java/com/bootcamp/Thi3/cau1/SecureCalculatorThi.java
@@ -7,7 +7,7 @@ public class SecureCalculatorThi {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("=== MAY TINH ===\n");
+        System.out.println("=== MAY TINH DA NANG ===\n");
 
         int a = 0;
         while (true) {
